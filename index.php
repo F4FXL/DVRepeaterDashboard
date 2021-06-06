7e242b0d84712dd5c1c85aa714ce1cccf93374d8
f4fGeoffrey Merck <f4fxl@users.noreply.github.com>
2021-06-06 07:57:19 +0200

    trying to fix bootstraptable breaking bootstrap behavior

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
     include_once("config.php");
 ?>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/botstraptablefixup.css">
     </head>
     <body>
         <div class="container-fluid" id="dashboard-content">
