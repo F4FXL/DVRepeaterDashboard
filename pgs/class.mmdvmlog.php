289909e8fc2918d1d19c935b26aa72b6c1a0fe0c
Geoffrey Merck <F4FXL@users.noreply.github.com>
2024-01-27 20:43:31 +0100

    Fix array error when no RSSI

--- pgs/class.mmdvmlog.php
+++ pgs/class.mmdvmlog.php
@@ -146,7 +146,7 @@ class MMDVMLog
             $heardItem->_source = $isRF? "RF" : "Net";
             $heardItem->_berorloss = $isRF? $matches[8][0] : $matches[7][0];
             $heardItem->_istxing = false;
-            $heardItem->_rssi = $matches[12][0];
+            if(count($matches) > 12) $heardItem->_rssi = $matches[12][0];
             
             return true;
         }
